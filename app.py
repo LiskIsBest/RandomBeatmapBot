61b5d7a3e641bce0a38c979db33139ac07919468
Darien Moore <Darienmoore2002@gmail.com>
2023-02-02 05:44:20 -0600

    added better logging

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from random import randint
+from datetime import datetime
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -9,25 +10,26 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=">>", intents=intents)
+bot.remove_command("help")
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TOKEN = os.getenv("TOKEN")
 ID_MAX = 5_000_000
 
-VALID_MODES = ["osu","mania","fruits","taiko"]
+VALID_MODES = ("osu","mania","fruits","taiko")
+
+@bot.before_invoke
+async def common(ctx):
+	author = ctx.message.author
+	print(f"USER:{author.name}, ID:{author.id}, COMMAND:{ctx.invoked_with}, TIME:{datetime.now().replace(microsecond=0)}")
 
 @bot.command()
-async def bothelp(ctx):
-	await ctx.send('Commands:\n>>givemap [osu, mania, taiko, fruits]\n\n')
+async def help(ctx):
+	await ctx.send('Commands:\n  >>givemap [osu, mania, taiko, fruits]\n  fruits = Catch the beat')
 
 @bot.command()
-async def givemap(ctx, arg=""):
-
-	if arg == "fruits":
-		print(f"{ctx.message.author.name} entered fruits")
-		await ctx.send("not enough ctb maps pick a different mode.")
-		return
+async def random(ctx, arg=""):
 	
 	if arg not in VALID_MODES:
 		print("Setting arg to None")
@@ -50,11 +52,10 @@ async def givemap(ctx, arg=""):
 				if arg == None:
 					...
 				elif beatmap.mode.value != arg:
-					print(beatmap.mode.value)
 					beatmap = None
 			except:
 				beatmap = None
-
+	print(f"FOUND:{beatmap.url}, MODE:{beatmap.mode.value}, STATUS:{beatmap.status.value}")
 	await ctx.send(f"{ctx.message.author.name} requested a random{' '+arg if arg != None else ''} beatmap: {beatmap.url}")
 	
 try:
