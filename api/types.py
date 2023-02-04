75969d2623252f6e30ddd12fd04b98fa96645260
Darien Moore <Darienmoore2002@gmail.com>
2023-02-04 01:24:30 -0600

    finished all types needed for beatmap

--- api/types.py
+++ api/types.py
@@ -1,6 +1,7 @@
 from enum import Enum
-# import datetime
+from datetime import datetime
 from pydantic import BaseModel, Field
+from typing import Optional
 
 class GameMode(Enum):
     OSU = "osu"
@@ -18,9 +19,122 @@ class Covers(BaseModel):
     slimcover: str
     slimcover: str = Field(alias="slimcover@2x")
     
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
     
+class Availability(BaseModel):
+    download_disabled: bool
+    more_information: Optional[str]
+    
+class Hype(BaseModel):
+    current: int
+    required: int
+
+class Nominations(BaseModel):
+    current: int
+    required: int
+
+class Failtimes(BaseModel):
+    exit: Optional[list[int]]
+    fail: Optional[list[int]]
+
 class BeatmapsetCompact(BaseModel):
     artist: str
     artist_unicode: str
-    covers
+    covers: Covers
+    creator: str
+    favorite_count: str = Field(alias="favourite_count")
+    id: int
+    nsfw: bool
+    play_count: int
+    preview_url: str
+    source: str
+    status: str
+    title: str
+    title_unicode: str
+    user_id: int
+    video: bool
+    ratins: list[int]
 
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
+
+class Beatmapset(BeatmapsetCompact):
+    availability: Availability
+    bpm: float
+    can_be_hyped: bool
+    creator: str
+    discussion_locked: bool
+    hype: Optional[Hype]
+    is_scoreable: bool
+    last_updated: datetime
+    legacy_thread_url: Optional[str]
+    nominations: Nominations = Field(alias="nominations_summary")
+    ranked: int
+    ranked_date = Optional[datetime]
+    source: str
+    storyboard: bool
+    submitted_date: Optional[datetime]
+    tags: str
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
+
+class BeatmapCompact(BaseModel):
+    beatmapset_id: int
+    difficulty_rating: float
+    id: int
+    mode: str
+    status: str
+    total_length: int
+    user_id: int
+    version: str
+    beatmapset: Optional[Beatmapset|BeatmapsetCompact]
+    checksum: Optional[str]
+    failtimes: Optional[Failtimes]
+    max_combo: Optional[int]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
+    
+class Beatmap(BeatmapCompact):
+    accuracy: float
+    ar: float
+    beatmapset_id: int
+    bpm: Optional[float]
+    covert: bool
+    count_circles: int
+    count_sliders: int
+    count_spinners: int
+    cs: float
+    deleted_at: Optional[datetime]
+    drain: float
+    hit_length: int
+    is_scoreable: bool
+    last_updated: datetime
+    mode_int: int
+    passcount: int
+    playcount: int
+    ranked: int
+    url: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+                datetime: str,
+                }
